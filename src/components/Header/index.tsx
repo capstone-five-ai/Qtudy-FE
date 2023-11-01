afe0958145cc7aaa751cdd500507ccc9470e2e5a
aaminha <dks4857@gmail.com>
2023-11-02 03:41:35 +0900

    [#5] feat: 헤더 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import Wrapper from '../Wrapper/Wrapper';
+import MenuBar from './MenuBar';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+`;
+
+function Header() {
+  return (
+    <Wrapper>
+      <Container>
+        <img alt="main-logo" src="/src/assets/logo/logo_main.svg" width="88px" />
+        <MenuBar />
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Header;
