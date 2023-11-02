dbfb7bba077afd861a6bb200eb204104ec9fbe7f
aaminha <dks4857@gmail.com>
2023-11-03 01:23:20 +0900

    [#8] feature: ContentHeader 컴포넌트 구현 * ContentHeader : 페이지 설명 문구를 보여주는 컴포넌트

---
+++ src/components/Header/ContentHeader.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { PageContentType } from '../../types';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  text-align: center;
+  padding: 20px 0px;
+
+  .main-text {
+    font-family: NotoSansBold;
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.grayScale02};
+  }
+
+  .sub-text {
+    font-family: NotoSansMedium;
+    font-size: 12px;
+    color: ${(props) => props.theme.colors.grayScale03};
+  }
+`;
+
+function ContentHeader({ text }: { text: PageContentType }) {
+  return (
+    <Container>
+      <div className="main-text">{text.main}</div>
+      <div className="sub-text">{text.sub}</div>
+    </Container>
+  );
+}
+
+export default ContentHeader;
