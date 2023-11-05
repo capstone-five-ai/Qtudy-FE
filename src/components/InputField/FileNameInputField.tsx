7ff8b4a937b04b0df94a1492216335a5fa07f51b
aaminha <dks4857@gmail.com>
2023-11-06 05:05:57 +0900

    [#10] feat: FileNameInputField 컴포넌트 구현

---
+++ src/components/InputField/FileNameInputField.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import InputField from './InputField';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  .title {
+    font-family: NotoSansMedium;
+    color: ${(props) => props.theme.colors.grayScale02};
+  }
+`;
+
+function FileNameInputField({ ...props }) {
+  return (
+    <Container>
+      <div className="title">파일명</div>
+      <InputField {...props} />
+    </Container>
+  );
+}
+
+export default FileNameInputField;
