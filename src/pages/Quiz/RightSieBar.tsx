53ff237ef3e4e9152385cc7a1eb6cafacdb9e3da
aaminha <dks4857@gmail.com>
2023-11-11 16:35:59 +0900

    feat: RightSideBar 컴포넌트 초안

--- src/pages/Quiz/RightSieBar.tsx
+++ src/pages/Quiz/RightSieBar.tsx
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import RadioButtonList from '../../components/Button/RadioButtonList';
+import LargeButton from '../../components/Button/LargeButton';
+import FileNameInputField from '../../components/InputField/FileNameInputField';
 
 const Container = styled.div`
   box-sizing: border-box;
   width: 360px;
   min-height: calc(100vh - 254px);
   padding: 24px 0px;
-
-  background-color: yellow;
 `;
 
 const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
   height: 100%;
   padding: 0px 36px;
 
@@ -19,13 +23,28 @@ const InnerContainer = styled.div`
   border-color: ${(props) => props.theme.colors.grayScale06};
 `;
 
+const LIST = [
+  { label: '문제 유형', button: ['객관식', '주관식'] },
+  { label: '문제량', button: ['적게', '적당히', '많이'] },
+  { label: '난이도', button: ['상', '중', '하'] },
+];
+
 function RightSideBar() {
-  const list = ['객관식', '주관식'];
-  const [checked, setChecked] = useState<string>(list[0]);
+  const [checked, setChecked] = useState<string>('');
   return (
     <Container>
       <InnerContainer>
-        <RadioButtonList buttonLabel="문제 유형" buttonList={list} checked={checked} setChecked={setChecked} />
+        {LIST.map((list) => (
+          <RadioButtonList
+            buttonLabel={list.label}
+            buttonList={list.button}
+            checkedValue={checked}
+            setCheckedValue={setChecked}
+            disabled={false}
+          />
+        ))}
+        <FileNameInputField />
+        <LargeButton type="button">생성하기</LargeButton>
       </InnerContainer>
     </Container>
   );
