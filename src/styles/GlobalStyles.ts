493afb7ad341f7753f06c259beb3acec8e14d9f4
callmejeje <choije401@naver.com>
2023-11-18 17:26:54 +0900

    feat: 나눔스퀘어 bold, extrabold 추가

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import NanumSquareBold from '../assets/fonts/NanumSquareNeoTTF-cBd.woff';
+import NanumSquareExtraBold from '../assets/fonts/NanumSquareNeoTTF-dEb.woff';
 import NotoSansBold from '../assets/fonts/NotoSansKR-Bold.woff';
 import NotoSansMedium from '../assets/fonts/NotoSansKR-Medium.woff';
 import NotoSansRegular from '../assets/fonts/NotoSansKR-Regular.woff';
@@ -20,6 +22,18 @@ const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: NotoSansBold;
     src: url(${NotoSansBold}) format('woff');
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: NanumSquareBold;
+    src: url(${NanumSquareBold}) format('woff');
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: NanumSquareExtraBold;
+    src: url(${NanumSquareExtraBold}) format('woff');
     font-style: normal;
   }
 `;
