bb968b432c11824334777231e97277e234edea5a
aaminha <dks4857@gmail.com>
2023-11-27 00:09:17 +0900

    [#16] feat: Loader 컴포넌트 적용 여부를 위한 loadingState 적용

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import Header from '../components/Header';
+import Loader from '../components/Loader';
+import loadingSelector from '../recoil/selectors/loading';
 
 function Layout() {
+  const loading = useRecoilValue(loadingSelector);
+
   return (
     <>
+      {loading && <Loader />}
       <Header />
       <Outlet />
     </>
