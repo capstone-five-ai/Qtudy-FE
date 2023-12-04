3e774f1a2b7f62f95992c919a21b927bb00ea66b
aaminha <dks4857@gmail.com>
2023-12-04 10:36:00 +0900

    [#24] feat: AI 요약 정리 생성 오른쪽 사이드바 구현

---
+++ src/pages/Summary/AISummary/CreateAISummary/RightSideBar.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import RadioButtonFieldList from '../../../../components/Button/RadioButton/RadioButtonFieldList';
+import FileNameInputField from '../../../../components/Input/FileNameInputField';
+import SideBar from '../../../../components/SideBar';
+
+const LIST = [{ dataKey: 'amount', label: '요약량', button: ['짧게', '적당히', '길게'] }];
+
+interface RightSideBarProps {
+  disabled?: boolean; // 사이드바 활성 여부
+  inputOption: { [key: string]: string }; // 설정 변수
+  setInputOption: React.Dispatch<React.SetStateAction<{ [key: string]: string }>>; // 설정 수정 함수
+  handleSubmit: () => void;
+}
+
+function RightSideBar({ disabled = false, inputOption, setInputOption, handleSubmit }: RightSideBarProps) {
+  const handleFileNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputOption({ ...inputOption, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <SideBar buttonDisabled={Object.values(inputOption).includes('')} handleSubmit={handleSubmit}>
+      <Container>
+        {LIST.map((list) => (
+          <RadioButtonFieldList
+            key={list.dataKey}
+            optionInputKey={list.dataKey}
+            buttonLabel={list.label}
+            buttonList={list.button}
+            inputOption={inputOption}
+            setInputOption={setInputOption}
+            disabled={disabled}
+          />
+        ))}
+        <FileNameInputField name="file" value={inputOption.file} onChange={handleFileNameChange} disabled={disabled} />
+      </Container>
+    </SideBar>
+  );
+}
+
+RightSideBar.defaultProps = {
+  disabled: false,
+};
+
+export default RightSideBar;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
