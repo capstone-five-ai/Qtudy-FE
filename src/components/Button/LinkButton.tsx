05e7c8a458ff32a7660914ed73994f759d5b745d
callmejeje <choije401@naver.com>
2023-12-09 11:17:21 +0900

    [#27] feat: 링크 공유 버튼 컴포넌트 구현

---
+++ src/components/Button/LinkButton.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+import { ReactComponent as LinkIcon } from '../../assets/icons/link_gradation.svg';
+import ShareLinkModal from '../Modal/ShareLinkModal';
+import Typography from '../Typography';
+
+type Props = {
+  link: string;
+};
+
+function LinkButton({ link }: Props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickLink = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <>
+      <Wrapper onClick={handleClickLink}>
+        <LinkIcon />
+        <Typography variant="caption3" color="grayScale03">
+          링크 공유
+        </Typography>
+      </Wrapper>
+
+      {showModal && <ShareLinkModal onClose={() => setShowModal(false)} link={link} />}
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  cursor: pointer;
+`;
+
+export default LinkButton;
