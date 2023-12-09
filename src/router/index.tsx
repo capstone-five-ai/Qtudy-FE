69e0eb7612f238f769e52bb9ee3fb4a4998a5038
callmejeje <choije401@naver.com>
2023-12-10 01:51:21 +0900

    [#43] refactor: router 폴더 구조 변경

---
+++ src/router/index.tsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../layouts';
+import Management from '../pages/Management';
+import Quiz from '../pages/Quiz';
+import Summary from '../pages/Summary';
+import Login from '../pages/login';
+import Redirection from '../pages/login/Redirection';
+import SelectService from '../pages/selectService';
+import ProtectedRoute from './ProtectedRoute';
+
+const routes = [
+  {
+    path: '/',
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: '/',
+        element: <Layout />,
+        children: [
+          {
+            path: 'quiz/*',
+            element: <Quiz />,
+          },
+          { path: 'summary/*', element: <Summary /> },
+          {
+            path: 'management/*',
+            element: <Management />,
+          },
+          { path: 'select', element: <SelectService /> },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'oauth/kakao/callback',
+    element: <Redirection />,
+  },
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'quiz/*',
+        element: <Quiz />,
+      },
+      { path: 'summary/*', element: <Summary /> },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+export default router;
