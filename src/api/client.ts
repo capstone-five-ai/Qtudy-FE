aa2b919bbb9cf083bb2dc9427ad09fc3ee5e7eb8
callmejeje <choije401@naver.com>
2023-12-10 15:53:19 +0900

    [#43] feat: 리프레시 토큰 발급 인터셉터 구현

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,6 +4,47 @@ const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}`,
 });
 
-export default apiClient;
+const updateToken = async () => {
+  const response = await apiClient.post('api/access-token/issue', null, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('refreshToken')}`,
+    },
+  });
 
-// TODO: apiClient.interceptors 구현
+  return response;
+};
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    if (status === 401) {
+      const originRequest = config;
+      const response = await updateToken();
+
+      if (response.status === 200) {
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem('accessToken', newAccessToken);
+        originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axios(originRequest);
+      }
+      if (response.status === 401) {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+
+        window.location.replace('/login');
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
