b4726e5bbae469201085ccdae08c95eca10012cb
callmejeje <choije401@naver.com>
2023-12-12 08:13:34 +0900

    [#51] feat: api path 수정

--- src/api/CategoryApi.ts
+++ src/api/CategoryApi.ts
@@ -6,7 +6,7 @@ const SIZE = 100;
 const CategoryApi = {
   getCategorys: async (categoryType: string) => {
     // 카테고리(Category)/카테고리 목록 조회
-    const response = await apiClient.get('category/list', {
+    const response = await apiClient.get('api/category/list', {
       params: {
         page: PAGE,
         size: SIZE,
@@ -18,7 +18,7 @@ const CategoryApi = {
 
   getCategoryItems: async (categoryId: number) => {
     // 카테고리(Category)/카테고리 단건 조회
-    const response = await apiClient.get(`category/${categoryId}`, {
+    const response = await apiClient.get(`api/category/${categoryId}`, {
       params: {
         page: PAGE,
         size: SIZE,
@@ -29,7 +29,7 @@ const CategoryApi = {
 
   createCategory: async (categoryName: string, categoryType: string) => {
     // 카테고리(Category)/카테고리 생성
-    const response = await apiClient.post('category/new', {
+    const response = await apiClient.post('api/category/new', {
       categoryName,
       categoryType,
     });
@@ -38,7 +38,7 @@ const CategoryApi = {
 
   editCategory: async (categoryId: number, categoryName: string) => {
     // 카테고리(Category)/카테고리 수정
-    const response = await apiClient.patch(`category/edit/${categoryId}`, {
+    const response = await apiClient.patch(`api/category/edit/${categoryId}`, {
       categoryName,
     });
     return response.data;
@@ -46,7 +46,7 @@ const CategoryApi = {
 
   deleteCategory: async (categoryId: number) => {
     // 카테고리(Category)/카테고리 삭제
-    const response = await apiClient.delete(`category/edit/${categoryId}`);
+    const response = await apiClient.delete(`api/category/edit/${categoryId}`);
     return response.data;
   },
 };
