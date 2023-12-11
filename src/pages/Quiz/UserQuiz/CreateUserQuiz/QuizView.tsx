9596878ac324557b4a6862f853eecadde59e80f8
aaminha <dks4857@gmail.com>
2023-12-11 15:54:05 +0900

    [#60] fix: 자체 퀴즈 생성 전체적인 구조 및 로직 수정, 버그 수정

---
+++ src/pages/Quiz/UserQuiz/CreateUserQuiz/QuizView.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { CREATE_USER_QUIZ_TYPE } from '../../../../constants';
+import { UserQuizInputType } from '../../../../types';
+import QuestionField from './QuestionField';
+import OptionField from './OptionField';
+import AddOptionButton from '../../../../components/Button/AddOptionButton';
+
+interface QuizViewProps {
+  quizType: string;
+  question: UserQuizInputType;
+  options: UserQuizInputType[];
+  answer: number;
+  setQuestion: React.Dispatch<React.SetStateAction<UserQuizInputType>>;
+  setOptions: React.Dispatch<React.SetStateAction<UserQuizInputType[]>>;
+  setAnswer: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const DEFAULT_INPUT = { input: '', check: false };
+const MAX_OPTION_COUNT = 10;
+
+function QuizView({ quizType, question, options, answer, setQuestion, setOptions, setAnswer }: QuizViewProps) {
+  const handleAddOption = () => {
+    setOptions([...options, DEFAULT_INPUT]);
+  };
+
+  return (
+    <div>
+      {quizType === CREATE_USER_QUIZ_TYPE[0].label ? (
+        <QuizInputFieldContainer>
+          <QuestionField question={question} setQuestion={setQuestion} />
+          <OptionField options={options} answer={answer} setOptions={setOptions} setAnswer={setAnswer} />
+          <AddOptionButton handleClick={handleAddOption} disabled={options.length >= MAX_OPTION_COUNT} />
+        </QuizInputFieldContainer>
+      ) : (
+        <QuestionField question={question} setQuestion={setQuestion} />
+      )}
+    </div>
+  );
+}
+
+export default QuizView;
+
+const QuizInputFieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
