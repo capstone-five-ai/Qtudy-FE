2712537ad63f9ec4839c807f630675044384148d
jueun_choi <jueun_choi2@tmax.co.kr>
2023-12-12 19:46:45 +0900

    [#51] feat: pdfbutton props 추가

--- src/pages/Management/History/HistoryList/HistoryItem/index.tsx
+++ src/pages/Management/History/HistoryList/HistoryItem/index.tsx
@@ -88,11 +88,32 @@ function HistoryItem({ history, updateList }: Props) {
       <PDFDown>
         {history.dtype === 'PROBLEM' ? (
           <>
-            <PDFButton label="퀴즈" variant={2} fileId={history.fileId} pdfType="PROBLEM" type="ai" />
-            <PDFButton label="정답" variant={2} fileId={history.fileId} pdfType="ANSWER" type="ai" />
+            <PDFButton
+              label="퀴즈"
+              variant={2}
+              fileId={history.fileId}
+              pdfType="PROBLEM"
+              type="ai"
+              fileName={history.fileName}
+            />
+            <PDFButton
+              label="정답"
+              variant={2}
+              fileId={history.fileId}
+              pdfType="ANSWER"
+              type="ai"
+              fileName={history.fileName}
+            />
           </>
         ) : (
-          <PDFButton label="요약" variant={2} fileId={history.fileId} pdfType="SUMMARY" type="ai" />
+          <PDFButton
+            label="요약"
+            variant={2}
+            fileId={history.fileId}
+            pdfType="SUMMARY"
+            type="ai"
+            fileName={history.fileName}
+          />
         )}
       </PDFDown>
       <Delete>
