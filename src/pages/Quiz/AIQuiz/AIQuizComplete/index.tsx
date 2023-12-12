73917c93cc58326fbcadc03f3f0ab21994d0901f
jueun_choi <jueun_choi2@tmax.co.kr>
2023-12-12 19:47:08 +0900

    [#51] feat: ai 퀴즈 api 연결

--- src/pages/Quiz/AIQuiz/AIQuizComplete/index.tsx
+++ src/pages/Quiz/AIQuiz/AIQuizComplete/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
 import QuizApi from '../../../../api/QuizApi';
 import LinkButton from '../../../../components/Button/LinkButton';
 import PDFButton from '../../../../components/Button/PDFButton';
@@ -9,50 +10,46 @@ import CategoryModal from '../../../../components/Modal/CategoryModal';
 import Question from '../../../../components/Question';
 import { QuestionType } from '../../../../types/question.type';
 import NumberPannel from './NumberPannel';
+import authState from '../../../../recoil/atoms/authState';
 
 function AIQuizComplete() {
-  // AI 퀴즈의 경우 문제 번호에 따라 보여줘야 함
   const [qs] = useSearchParams();
   const fileId = Number(qs.get('id'));
+  const fileName = Number(qs.get('fileName'));
 
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [questionNum, setQuestionNum] = useState(1);
   const [isWriter, setIsWriter] = useState(false);
+  const isAuthenticated = useRecoilValue(authState);
 
   const [showCategoryModal, setShowCategoryModal] = useState(false);
   const link = window.location.href;
 
-  const [question, setQuestion] = useState<QuestionType>({
-    problemName: '인공지능은 무엇을 모방할 수 있는 기술 및 연구 분야인가요?',
-    problemAnswer: null,
-    problemCommentary:
-      '인공지능의 목표는 인간의 인지 능력을 모방할 수 있는 지능적인 기계를 만드는 것입니다. 즉, 사람처럼 생각하고 학습하며 문제를 해결할 수 있는 기계를 개발하는 것이 목표입니다.',
-    problemType: 'SUBJECTIVE',
-    problemChoices: ['선지1', '선지2', '선지3', '선지4'],
-  });
+  const [question, setQuestion] = useState<QuestionType>();
 
-  const getQuizList = async (id: number) => {
-    const data = await QuizApi.getAllAIQuiz(id);
-    console.log(data.problems);
+  const getQuizList = useCallback(
+    async (id: number) => {
+      const data = await QuizApi.getAllAIQuiz(id, isAuthenticated);
 
-    setQuestions(data.problems);
-    setQuestion(data.problems[0]);
+      setQuestions(data.problems);
+      setQuestion(data.problems[0]);
 
-    if (data.isWriter) setIsWriter(true);
-  };
+      if (data.isWriter) setIsWriter(true);
+    },
+    [isAuthenticated]
+  );
 
   useEffect(() => {
-    // TODO: questions api call
-    // TODO: 회원(작성자) / 회원(작성자 외) / 비회원 구분하여 화면 렌더링
-    //
+    if (!fileId) return;
     getQuizList(fileId);
-  }, [fileId]);
+  }, [fileId, getQuizList]);
 
   useEffect(() => {
-    // TODO: quiestionNum에 따라 setQuestion
-    //
+    if (!fileId) return;
     setQuestion(questions[questionNum - 1]); //
-  }, [questionNum, questions]);
+  }, [fileId, questionNum, questions]);
+
+  if (!question) return <div />;
 
   return (
     <>
@@ -63,13 +60,19 @@ function AIQuizComplete() {
       <SideWrapper>
         <SideBar>
           <div>
-            <NumberPannel numOfQuiz={28} questionNum={questionNum} setQuestionNum={setQuestionNum} />
+            <NumberPannel numOfQuiz={questions.length} questionNum={questionNum} setQuestionNum={setQuestionNum} />
             <ButtonWrapper>
               <LinkButton link={link} />
               {isWriter && (
                 <PDFWrapper>
-                  <PDFButton label="퀴즈" fileId={fileId} pdfType="PROBLEM" type="ai" />
-                  <PDFButton label="정답" fileId={fileId} pdfType="ANSWER" type="ai" />
+                  <PDFButton
+                    label="퀴즈"
+                    fileId={fileId}
+                    pdfType="PROBLEM"
+                    type="ai"
+                    fileName={`${fileName}_PROBLEM`}
+                  />
+                  <PDFButton label="정답" fileId={fileId} pdfType="ANSWER" type="ai" fileName={`${fileName}_ANSWER`} />
                 </PDFWrapper>
               )}
             </ButtonWrapper>
@@ -80,7 +83,14 @@ function AIQuizComplete() {
           </TwinkleButton>
         </SideBar>
       </SideWrapper>
-      {showCategoryModal && <CategoryModal onClose={() => setShowCategoryModal(false)} categoryType="PROBLEM" />}
+      {showCategoryModal && (
+        <CategoryModal
+          onClose={() => setShowCategoryModal(false)}
+          categoryType="PROBLEM"
+          contentId={question.aiGeneratedProblemId || -1}
+          generateType="ai"
+        />
+      )}
     </>
   );
 }
