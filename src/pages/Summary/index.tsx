7eb7ebd8fc3ad2c08c2224217cdaa80c3530a6d1
callmejeje <choije401@naver.com>
2023-12-13 01:32:49 +0900

    [#70] fix: 요약 정리 생성 페이지 회원만 접근 가능하도록 수정

--- src/pages/Summary/index.tsx
+++ src/pages/Summary/index.tsx
@@ -1,7 +1,9 @@
-import { Route, Routes, useSearchParams } from 'react-router-dom';
+import { Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import ContentWrapper from '../../components/Wrapper/ContentWrapper';
 import MainWrapper from '../../components/Wrapper/MainWrapper';
 import MainLayout from '../../layouts/MainLayout';
+import authState from '../../recoil/atoms/authState';
 import CreateAISummary from './AISummary/CreateAISummary';
 import SummaryComplete from './SummaryComplete';
 import CreateUserSummary from './UserSummary/CreateUserSummary';
@@ -9,6 +11,10 @@ import CreateUserSummary from './UserSummary/CreateUserSummary';
 function Summary() {
   const [qs] = useSearchParams();
   const complete = qs.get('complete') === 'true';
+
+  const isAuthenticated = useRecoilValue(authState);
+
+  if (!complete && !isAuthenticated) return <Navigate to="/login" replace />;
 
   return (
     <MainLayout contentKey="createSummary">
