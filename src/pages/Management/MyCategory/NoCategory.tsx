128d8a2462cbcf5e2f4265ef5d92df883099e1b6
aaminha <dks4857@gmail.com>
2023-12-28 11:57:07 +0900

    rename: `icon-empty` -> `empty` 파일명 변경

--- src/pages/Management/MyCategory/NoCategory.tsx
+++ src/pages/Management/MyCategory/NoCategory.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ReactComponent as EmptyIcon } from '../../../assets/icons/icon-empty.svg';
+import { ReactComponent as EmptyIcon } from '../../../assets/icons/empty.svg';
 import Typography from '../../../components/Typography';
 import DefaultButton from '../../../components/Button/DefaultButton';
 
