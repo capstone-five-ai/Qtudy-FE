942a0770fafbe2725bfffb6e34abe3476e0e2963
callmejeje <choije401@naver.com>
2024-03-03 21:46:56 +0900

    fix: headers의 Authrization 인터셉터에서 일괄적으로 처리, Authorization 없는 요청은 noAuthClient 사용

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -1,15 +1,15 @@
-import apiClient from './client';
+import apiClient, { noAuthClient } from './client';
 
 const AuthApi = {
   authKakao: async (code: string) => {
-    const response = await apiClient.get(
+    const response = await noAuthClient.get(
       `oauth/kakao/callback?code=${code}&redirectUri=${import.meta.env.VITE_REDIRECT_URI}`
     );
     return response.data;
   },
 
   login: async (accessToken: string) => {
-    const response = await apiClient.post(
+    const response = await noAuthClient.post(
       'api/oauth/login',
       {
         memberType: 'KAKAO',
@@ -24,11 +24,7 @@ const AuthApi = {
   },
 
   logout: async () => {
-    const response = await apiClient.post('api/logout', null, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-      },
-    });
+    const response = await apiClient.post('api/logout', null);
 
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
