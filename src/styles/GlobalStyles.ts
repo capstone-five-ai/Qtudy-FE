508bfa82ad682ccb5621a138f15131bb127498c9
jueun_choi <jueun_choi2@tmax.co.kr>
2024-03-20 19:55:14 +0900

    [#85] chore: 폰트 woff2 파일 적용

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,10 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import NanumSquareBold from '../assets/fonts/NanumSquareNeoTTF-cBd.woff';
-import NanumSquareExtraBold from '../assets/fonts/NanumSquareNeoTTF-dEb.woff';
-import NotoSansBold from '../assets/fonts/NotoSansKR-Bold.woff';
-import NotoSansMedium from '../assets/fonts/NotoSansKR-Medium.woff';
-import NotoSansRegular from '../assets/fonts/NotoSansKR-Regular.woff';
+
+import NanumSquareBold from '../assets/fonts/NanumSquareNeo/NanumSquareNeoTTF-cBd.woff';
+import NanumSquareExtraBold from '../assets/fonts/NanumSquareNeo/NanumSquareNeoTTF-dEb.woff';
+
+import NanumSquareBold2 from '../assets/fonts/NanumSquareNeo/NanumSquareNeoTTF-cBd.woff2';
+import NanumSquareExtraBold2 from '../assets/fonts/NanumSquareNeo/NanumSquareNeoTTF-dEb.woff2';
+
+import NotoSansBold from '../assets/fonts/NotoSansKR/NotoSansKR-Bold.woff';
+import NotoSansMedium from '../assets/fonts/NotoSansKR/NotoSansKR-Medium.woff';
+import NotoSansRegular from '../assets/fonts/NotoSansKR/NotoSansKR-Regular.woff';
+
+import NotoSansBold2 from '../assets/fonts/NotoSansKR/NotoSansKR-Bold.woff2';
+import NotoSansMedium2 from '../assets/fonts/NotoSansKR/NotoSansKR-Medium.woff2';
+import NotoSansRegular2 from '../assets/fonts/NotoSansKR/NotoSansKR-Regular.woff2';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
@@ -23,31 +32,36 @@ const GlobalStyles = createGlobalStyle`
 
   @font-face {
     font-family: NotoSansRegular;
-    src: url(${NotoSansRegular}) format('woff');
+    src: url(${NotoSansRegular2}) format('woff2'),
+      url(${NotoSansRegular}) format('woff');
     font-style: normal;
   }
 
   @font-face {
     font-family: NotoSansMedium;
-    src: url(${NotoSansMedium}) format('woff');
+    src: url(${NotoSansMedium2}) format('woff2'),
+      url(${NotoSansMedium}) format('woff');
     font-style: normal;
   }
   
   @font-face {
     font-family: NotoSansBold;
-    src: url(${NotoSansBold}) format('woff');
+    src: url(${NotoSansBold2}) format('woff2'),
+      url(${NotoSansBold}) format('woff');
     font-style: normal;
   }
 
   @font-face {
     font-family: NanumSquareBold;
-    src: url(${NanumSquareBold}) format('woff');
+    src: url(${NanumSquareBold2}) format('woff2'),
+      url(${NanumSquareBold}) format('woff');
     font-style: normal;
   }
 
   @font-face {
     font-family: NanumSquareExtraBold;
-    src: url(${NanumSquareExtraBold}) format('woff');
+    src: url(${NanumSquareExtraBold2}) format('woff2'),
+      url(${NanumSquareExtraBold}) format('woff');
     font-style: normal;
   }
 `;
