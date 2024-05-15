dd931f5666ba450fc9ccb3dd3566231cb1a4e34b
callmejeje <choije401@naver.com>
2024-05-15 18:55:06 +0900

    [#94] feat: 빈 히스토리 퀴즈, 요약 분리

--- src/pages/Management/History/EmptyHistory/index.tsx
+++ src/pages/Management/History/EmptyHistory/index.tsx
@@ -1,16 +1,32 @@
 import { useNavigate } from 'react-router';
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 import DefaultButton from '../../../../components/Button/DefaultButton';
 import Empty1 from '../../../../components/Empty/Empty1';
 
-function EmptyHistory() {
+interface Props {
+  type: 'all' | 'quiz' | 'summary';
+}
+
+function EmptyHistory({ type }: Props) {
   const navigate = useNavigate();
+  const highlight = (function getHighlight() {
+    if (type === 'quiz') return 'AI 퀴즈';
+    if (type === 'summary') return 'AI 요약';
+    return 'AI 퀴즈 및 요약';
+  })();
+
+  const title = `아직 생성된 ${highlight}${type === 'quiz' ? '가' : '이'} 없어요`;
+
   return (
     <Container>
-      <Empty1 title="아직 생성된 AI 퀴즈 및 요약이 없어요" highlight="AI 퀴즈 및 요약" />
+      <Empty1 title={title} highlight={highlight} />
       <ButtonContainer>
-        <DefaultButton onClick={() => navigate('/quiz/ai')}>AI 퀴즈 생성하러 가기</DefaultButton>
-        <DefaultButton onClick={() => navigate('/summary/ai')}>AI 요약 생성하러 가기</DefaultButton>
+        {(type === 'all' || type === 'quiz') && (
+          <DefaultButton onClick={() => navigate('/quiz/ai')}>AI 퀴즈 생성하러 가기</DefaultButton>
+        )}
+        {(type === 'all' || type === 'summary') && (
+          <DefaultButton onClick={() => navigate('/summary/ai')}>AI 요약 생성하러 가기</DefaultButton>
+        )}
       </ButtonContainer>
     </Container>
   );
